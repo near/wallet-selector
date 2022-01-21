b20e53622e1a44e8a7a05c2a4634bdf1404a4c2c
Kujtim.Prenku <Kujtim.Prenku@sqa-consulting.com>
2022-01-21 12:08:30 +0100

    fixed styles when the system/browser is in dark mode

--- src/modal/component/Modal/Modal.tsx
+++ src/modal/component/Modal/Modal.tsx
@@ -28,15 +28,6 @@ function Modal(): JSX.Element {
     };
   }, []);
 
-  const mountedStyle = {
-    animation: "inAnimation 350ms ease-in",
-    maxHeight: "300px",
-  };
-  const unmountedStyle = {
-    animation: "outAnimation 200ms ease-out",
-    animationFillMode: "forwards",
-  };
-
   function handleCloseModal(event: any) {
     event.preventDefault();
     if (event.target === event.currentTarget) onCloseModalHandler();
@@ -158,6 +149,7 @@ function Modal(): JSX.Element {
               )}
               {useCustomDerivationPath && (
                 <input
+                  autoFocus
                   className={ledgerWalletError ? "input-error" : ""}
                   type="text"
                   placeholder="custom derivation path"
@@ -274,8 +266,11 @@ function Modal(): JSX.Element {
                 What is a Wallet?
               </span>
               <div
-                className="info-description"
-                style={walletInfoVisible ? mountedStyle : unmountedStyle}
+                className={
+                  walletInfoVisible
+                    ? `info-description show-explanation`
+                    : `info-description hide-explanation`
+                }
               >
                 <p>{state.options.walletSelectorUI.explanation}</p>
               </div>
