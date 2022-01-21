15f8b975d917c4770b85b01de31501b895b9f92c
amirsaran3 <amir.saran@sqa-consulting.com>
2022-01-21 15:59:15 +0100

    Finished adding account id input in ledger wallet option

--- src/interfaces/ILedgerWallet.ts
+++ src/interfaces/ILedgerWallet.ts
@@ -3,5 +3,6 @@ import IWallet from "./IWallet";
 export default interface ILedgerWallet extends IWallet {
   setDerivationPath(derivationPath: string): void;
   getPublicKey(): Uint8Array;
+  setAccountId(accountId: string): void;
   encodePublicKey(publicKey: Uint8Array): string;
 }
