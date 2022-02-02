30f23e66f2c4e1088ca538ebb2d885037319e303
Rares Golea <rgolea@users.noreply.github.com>
2022-02-02 09:56:25 +0100

    Creating localStorage

---
+++ src/services/storage.service.ts
@@ -0,0 +1,50 @@
+import { Promisify } from "../utils/types";
+import { defer } from "../utils/HelperFunctions";
+
+export class StorageService implements Promisify<Storage> {
+  private readonly map = new Map<string, string>();
+
+  constructor(private readonly prefix: string) {
+    this.init();
+  }
+
+  private init() {
+    Object.keys(localStorage).forEach((key) => {
+      if (key.startsWith(this.prefix)) {
+        this.map.set(key.slice(this.prefix.length), localStorage.getItem(key)!);
+      }
+    });
+  }
+
+  async clear(): Promise<void> {
+    this.map.clear();
+    defer(() => {
+      localStorage.clear();
+    });
+  }
+  async getItem(key: string): Promise<string | null> {
+    return this.map.get(key) || null;
+  }
+
+  async key(index: number): Promise<string | null> {
+    return Object.keys(Object.fromEntries(this.map))[index] || null;
+  }
+
+  async setItem(key: string, value: string): Promise<void> {
+    this.map.set(key, value);
+    defer(() => {
+      localStorage.setItem(`${this.prefix}${key}`, value);
+    });
+  }
+
+  async removeItem(key: string): Promise<void> {
+    this.map.delete(key);
+    defer(() => {
+      localStorage.removeItem(`${this.prefix}${key}`);
+    });
+  }
+
+  get length(): number {
+    return this.map.size;
+  }
+}
--- src/utils/HelperFunctions.ts
+++ src/utils/HelperFunctions.ts
@@ -7,7 +7,18 @@ const HelperFunctions = {
   },
   isMobile() {
     return mobile();
-  },
+  }
 };
 
 export default HelperFunctions;
+
+export function delay(fn: Function, wait: number, args?: any[]) {
+  return setTimeout(() => {
+    // eslint-disable-next-line prefer-spread
+    return fn.apply(null, args);
+  }, wait);
+}
+
+export function defer(fn: Function, args?: any[]) {
+  return delay(fn, 0, args);
+}
