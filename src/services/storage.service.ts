116052b7175443f3edc2a9f983dcedf3b5e59581
Rares Golea <rgolea@users.noreply.github.com>
2022-02-02 11:00:27 +0100

    Changing localStorage to any type of storage implementation

--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -1,24 +1,27 @@
 import { Promisify } from "../utils/types";
 import { defer } from "../utils/HelperFunctions";
 
-export class StorageService implements Promisify<Storage> {
-  static instances = new Map<string, StorageService>();
+export class PersistentStorage implements Promisify<Storage> {
+  static instances = new Map<string, PersistentStorage>();
 
   private readonly map = new Map<string, string>();
 
-  constructor(private readonly prefix: string) {
-    if(StorageService.instances.has(prefix)) {
-      return StorageService.instances.get(prefix)!;
+  constructor(private readonly prefix: string, private readonly storage: Storage = window?.localStorage) {
+    if(!storage) {
+      throw new Error("No storage available");
     }
 
-    StorageService.instances.set(prefix, this);
+    if(PersistentStorage.instances.has(prefix)) {
+      return PersistentStorage.instances.get(prefix)!;
+    }
+    PersistentStorage.instances.set(prefix, this);
     this.init();
   }
 
   private init() {
-    Object.keys(localStorage).forEach((key) => {
+    Object.keys(this.storage).forEach((key) => {
       if (key.startsWith(this.prefix)) {
-        this.map.set(key.slice(this.prefix.length), localStorage.getItem(key)!);
+        this.map.set(key.slice(this.prefix.length), this.storage.getItem(key)!);
       }
     });
   }
@@ -26,7 +29,7 @@ export class StorageService implements Promisify<Storage> {
   async clear(): Promise<void> {
     this.map.clear();
     defer(() => {
-      localStorage.clear();
+      this.storage.clear();
     });
   }
   async getItem(key: string): Promise<string | null> {
@@ -40,14 +43,14 @@ export class StorageService implements Promisify<Storage> {
   async setItem(key: string, value: string): Promise<void> {
     this.map.set(key, value);
     defer(() => {
-      localStorage.setItem(`${this.prefix}${key}`, value);
+      this.storage.setItem(`${this.prefix}${key}`, value);
     });
   }
 
   async removeItem(key: string): Promise<void> {
     this.map.delete(key);
     defer(() => {
-      localStorage.removeItem(`${this.prefix}${key}`);
+      this.storage.removeItem(`${this.prefix}${key}`);
     });
   }
 
