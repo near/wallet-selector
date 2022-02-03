e954028e1b85832087d9b49f702df88eba20cbf1
Rares Golea <rgolea@users.noreply.github.com>
2022-02-03 12:26:08 +0100

    Tests fixes

--- src/services/persistent-storage.service.spec.ts
+++ src/services/persistent-storage.service.spec.ts
@@ -3,12 +3,15 @@ import { mock, mockReset } from "jest-mock-extended";
 import { defer } from "../utils/HelperFunctions";
 
 describe("PersistentStorage Unit Tests", () => {
+  const prefix = "prefix";
+  const key = "testKey";
+  const value = "testValue";
   let storage: Storage;
   let persistentStorage: PersistentStorage;
 
   beforeAll(() => {
     storage = mock<Storage>();
-    persistentStorage = new PersistentStorage("testing", storage);
+    persistentStorage = new PersistentStorage(prefix, storage);
   });
 
   afterEach(async () => {
@@ -24,43 +27,46 @@ describe("PersistentStorage Unit Tests", () => {
   });
 
   it("should instanciate as a singleton", () => {
-    const persistentStorage2 = new PersistentStorage("testing", storage);
+    const persistentStorage2 = new PersistentStorage(prefix, storage);
     expect(persistentStorage).toBe(persistentStorage2);
   });
 
   it('should init properly', async () => {
+    const initPrefix = "init";
+    const initKey = "testKey";
+    const initValue = "testValue";
     const initStorage = mock<Storage>({
-      getItem: jest.fn().mockImplementation(() => "testValue"),
+      getItem: jest.fn().mockImplementation(() => initValue),
       length: 1,
-      key: jest.fn().mockImplementation(() => "inittestKey"),
+      key: jest.fn().mockImplementation(() => `${initPrefix}${initKey}`),
     });
 
-    const initPersistantStorage = new PersistentStorage("init", initStorage);
+    const initPersistantStorage = new PersistentStorage(initPrefix, initStorage);
 
-    const value = await initPersistantStorage.getItem("testKey");
-    expect(value).toBe("testValue");
-    expect(initStorage.getItem).toHaveBeenCalledWith("inittestKey");
+    const found = await initPersistantStorage.getItem(initKey);
+    expect(found).toBe(initValue);
+    expect(initStorage.getItem).toHaveBeenCalledWith(`${initPrefix}${initKey}`);
     expect(initPersistantStorage.length).toBe(1);
 
   });
 
   it("should set and get a value and sync to localstorage", async () => {
-    await persistentStorage.setItem("test", "test");
-    const setValue = await persistentStorage.getItem("test");
-    expect(setValue).toBe("test");
+    await persistentStorage.setItem(key, value);
+    const setValue = await persistentStorage.getItem(key);
+    expect(setValue).toBe(value);
   });
 
   it("should defer setting the value to localstorage until after the JS stack has cleared (performance first)", async () => {
-    await persistentStorage.setItem("test", "test");
+    await persistentStorage.setItem(key, value);
     expect(storage.setItem).toBeCalledTimes(0);
     await defer(() => {});
     expect(storage.setItem).toBeCalledTimes(1);
   });
 
   it("should not retrieve value from the localstorage", async () => {
-    await persistentStorage.setItem("test", "test");
-    const value = await persistentStorage.getItem("test");
-    expect(value).toBe("test");
+    await persistentStorage.setItem(key, value);
+    const found = await persistentStorage.getItem(key);
+    expect(found).toBe(value);
     expect(storage.getItem).toBeCalledTimes(0);
     await defer(() => {});
     expect(storage.getItem).toBeCalledTimes(0);
@@ -85,22 +91,24 @@ describe("PersistentStorage Unit Tests", () => {
   });
 
   it("should remove item", async () => {
-    await persistentStorage.setItem("test", "test");
-    await persistentStorage.setItem("test2", "test");
-    await persistentStorage.removeItem("test2");
-    expect(await persistentStorage.getItem('test2')).toBe(null);
-    expect(await persistentStorage.getItem('test')).toBe('test');
+    const testKey = "testRemovalKey";
+    const testValue = "testRemovalValue";
+    await persistentStorage.setItem(key, value);
+    await persistentStorage.setItem(testKey, testValue);
+    await persistentStorage.removeItem(testKey);
+    expect(await persistentStorage.getItem(testKey)).toBe(null);
+    expect(await persistentStorage.getItem(key)).toBe(value);
   });
 
   it('should set a prefix on storage', async () => {
-    await persistentStorage.setItem('test', 'test');
+    await persistentStorage.setItem(key, value);
     await defer(() => {});
-    expect(storage.setItem).toBeCalledWith('testingtest', 'test');
+    expect(storage.setItem).toBeCalledWith(`${prefix}${key}`, value);
   });
 
   it('should get a key', async () => {
-    await persistentStorage.setItem('test', 'test');
-    const key = await persistentStorage.key(0);
-    expect(key).toEqual('test');
+    await persistentStorage.setItem(key, value);
+    const existing = await persistentStorage.key(0);
+    expect(existing).toEqual(key);
   });
 });
