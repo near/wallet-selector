7ded5e316bde5dfd7c99946750fc0e6ea5f380aa
GentBinaku <gent.binaku@sqa-consulting.com>
2022-02-04 15:32:44 +0100

    First Commit

---
+++ src/utils/Logger.ts
@@ -0,0 +1,31 @@
+import { LogInterface } from "../interfaces/LogInterface";
+
+export class Logger implements LogInterface {
+
+  public debug(msg: string, ...supportingData: any[]): void {
+    this.emitLogMessage("debug", msg, supportingData);
+  }
+  public info(msg: string, ...supportingData: any[]): void {
+    this.emitLogMessage("info", msg, supportingData);
+  }
+  public warn(msg: string, ...supportingData: any[]): void {
+    this.emitLogMessage("warn", msg, supportingData);
+  }
+  public error(msg: string, ...supportingData: any[]): void {
+    this.emitLogMessage("error", msg, supportingData);
+  }
+  public log(msg: string, ...supportingData: any[]): void {
+    this.emitLogMessage("log", msg, supportingData);
+  }
+  private emitLogMessage(
+    msgType: "debug" | "info" | "warn" | "error" | "log",
+    msg: string,
+    supportingData: any[]
+  ) {
+    if (supportingData.length > 0) {
+      console[msgType](msg, supportingData);
+    } else {
+      console[msgType](msg);
+    }
+  }
+}
---
+++ src/utils/__tests__/Logger.spec.ts
@@ -0,0 +1,54 @@
+import { Logger } from "../Logger" 
+
+describe("Some logging behavior", () => {
+  const log = console.log; // save original console.log function
+  const warn = console.warn;
+  const debug = console.debug
+  const error = console.error
+  const info = console.info
+  const logger = new Logger();
+  beforeEach(() => {
+    console.log = jest.fn(); // create a new mock function for each test
+    console.warn = jest.fn();
+    console.error = jest.fn();
+    console.debug = jest.fn();
+    console.info = jest.fn();
+    
+  });
+  afterAll(() => {
+    console.log = log; // restore original console.log after all tests
+    console.warn = warn
+    console.error = error
+    console.debug = debug
+    console.info = info
+  });
+  it("no log", () => {
+    // TODO: test something that should not log
+    expect(console.log).not.toHaveBeenCalled();
+  });
+  it("some log", () => {
+    // TODO: test something that should log
+    logger.log("something")
+    expect(console.log).toHaveBeenCalled();
+  });
+  it("some debug", () => {
+    // TODO: test something that should log
+    logger.debug("something")
+    expect(console.debug).toHaveBeenCalled();
+  });
+  it("some warn", () => {
+    // TODO: test something that should log
+    logger.warn("something")
+    expect(console.warn).toHaveBeenCalled();
+  });
+  it("some error", () => {
+    // TODO: test something that should log
+    logger.error("something")
+    expect(console.error).toHaveBeenCalled();
+  });
+  it("some info", () => {
+    // TODO: test something that should log
+    logger.info("something")
+    expect(console.info).toHaveBeenCalled();
+  });
+});
