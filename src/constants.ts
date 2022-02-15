20e706efb48b7cb7ed1012f3e4bb09602550f7b5
lewis-sqa <lewis.barnes@sqa-consulting.com>
2022-02-15 09:35:40 +0000

    Reference package.name.

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,7 @@
-export const PACKAGE_NAME = "near-wallet-selector";
-export const LOCAL_STORAGE_SELECTED_WALLET_ID = `${PACKAGE_NAME}:selectedWalletId`;
-export const LOCAL_STORAGE_LEDGER_WALLET_AUTH_DATA = `${PACKAGE_NAME}:ledger-wallet:authData`;
+import packageJson from "../package.json";
+
+export const LOCAL_STORAGE_SELECTED_WALLET_ID = `${packageJson.name}:selectedWalletId`;
+export const LOCAL_STORAGE_LEDGER_WALLET_AUTH_DATA = `${packageJson.name}:ledger-wallet:authData`;
 
 export const MODAL_ELEMENT_ID = "near-wallet-selector-modal";
 export const DEFAULT_DERIVATION_PATH = "44'/397'/0'/0'/1'";
