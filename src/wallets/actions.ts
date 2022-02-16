d665e487547574a9408709b84bb4b530a3bc6f83
lewis-sqa <lewis.barnes@sqa-consulting.com>
2022-02-16 14:57:45 +0000

    Added support for AddKey.

--- src/wallets/actions.ts
+++ src/wallets/actions.ts
@@ -37,12 +37,22 @@ export interface StakeAction {
   };
 }
 
+export type AddKeyPermission =
+  | "FullAccess"
+  | {
+      receiverId: string;
+      allowance?: string;
+      methodNames?: Array<string>;
+    };
+
 export interface AddKeyAction {
   type: "AddKey";
   params: {
     publicKey: string;
-    // TODO: Determine the serializable version of `AccessKey`.
-    accessKey: unknown;
+    accessKey: {
+      nonce?: number;
+      permission: AddKeyPermission;
+    };
   };
 }
 
@@ -69,6 +79,19 @@ export type Action =
   | AddKeyAction
   | DeleteKeyAction
   | DeleteAccountAction;
+
+const getAccessKey = (permission: AddKeyPermission) => {
+  if (permission === "FullAccess") {
+    return transactions.fullAccessKey();
+  }
+
+  const { receiverId, methodNames = [] } = permission;
+  const allowance = permission.allowance
+    ? new BN(permission.allowance)
+    : undefined;
+
+  return transactions.functionCallAccessKey(receiverId, methodNames, allowance);
+};
 
 export const transformActions = (actions: Array<Action>) => {
   return actions.map((action) => {
@@ -103,12 +126,15 @@ export const transformActions = (actions: Array<Action>) => {
           utils.PublicKey.from(publicKey)
         );
       }
-      // case "AddKey": {
-      //   const { publicKey, accessKey } = action.params;
-      //
-      //   // TODO: Figure out how to convert to an AccessKey.
-      //   return transactions.addKey(utils.PublicKey.from(publicKey), accessKey);
-      // }
+      case "AddKey": {
+        const { publicKey, accessKey } = action.params;
+
+        return transactions.addKey(
+          utils.PublicKey.from(publicKey),
+          // TODO: Use accessKey.nonce? near-api-js seems to think 0 is fine?
+          getAccessKey(accessKey.permission)
+        );
+      }
       case "DeleteKey": {
         const { publicKey } = action.params;
 
@@ -120,7 +146,7 @@ export const transformActions = (actions: Array<Action>) => {
         return transactions.deleteAccount(beneficiaryId);
       }
       default:
-        throw new Error(`Invalid action type '${action.type}'`);
+        throw new Error("Invalid action type");
     }
   });
 };
