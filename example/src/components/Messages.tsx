d10da713e7b959057cc6ade77b3bcf6d13af1cde
lewis-sqa <lewis.barnes@sqa-consulting.com>
2022-02-17 13:56:27 +0000

    Switch to project ESLint.

--- example/src/components/Messages.tsx
+++ example/src/components/Messages.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { Message } from "../interfaces";
 
 interface MessagesProps {
@@ -9,15 +9,15 @@ const Messages: React.FC<MessagesProps> = ({ messages }) => {
   return (
     <>
       <h2>Messages</h2>
-      {messages.map((message, i) =>
+      {messages.map((message, i) => (
         // TODO: format as cards, add timestamp
-        <p key={i} className={message.premium ? 'is-premium' : ''}>
-          <strong>{message.sender}</strong>:<br/>
+        <p key={i} className={message.premium ? "is-premium" : ""}>
+          <strong>{message.sender}</strong>:<br />
           {message.text}
         </p>
-      )}
+      ))}
     </>
   );
-}
+};
 
 export default Messages;
