de2a4b8ebef3c7df35a3ada8f4d97cc537128631
lewis-sqa <lewis.barnes@sqa-consulting.com>
2022-02-28 16:42:52 +0000

    Trimmed config modules.

--- examples/react/src/config.ts
+++ examples/react/src/config.ts
@@ -10,10 +10,6 @@ const getConfig = (env: string) => {
         walletUrl: "https://wallet.near.org",
         helperUrl: "https://helper.mainnet.near.org",
       };
-    // This is an example app so production is set to testnet.
-    // You can move production to mainnet if that is applicable.
-    case "production":
-    case "development":
     case "testnet":
       return {
         networkId: "testnet",
@@ -30,33 +26,8 @@ const getConfig = (env: string) => {
         walletUrl: "https://wallet.betanet.near.org",
         helperUrl: "https://helper.betanet.near.org",
       };
-    case "local":
-      return {
-        networkId: "local",
-        nodeUrl: "http://localhost:3030",
-        keyPath: `${process.env.HOME}/.near/validator_key.json`,
-        walletUrl: "http://localhost:4000/wallet",
-        contractName: CONTRACT_NAME,
-      };
-    case "test":
-    case "ci":
-      return {
-        networkId: "shared-test",
-        nodeUrl: "https://rpc.ci-testnet.near.org",
-        contractName: CONTRACT_NAME,
-        masterAccount: "test.near",
-      };
-    case "ci-betanet":
-      return {
-        networkId: "shared-test-staging",
-        nodeUrl: "https://rpc.ci-betanet.near.org",
-        contractName: CONTRACT_NAME,
-        masterAccount: "test.near",
-      };
     default:
-      throw Error(
-        `Unconfigured environment '${env}'. Can be configured in src/config.js.`
-      );
+      throw Error(`Invalid environment '${env}'.`);
   }
 };
 
