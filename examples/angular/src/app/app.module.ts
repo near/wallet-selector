94b37c2bd7624052b2242ba55d5cda0b291349fa
Kujtim.Prenku <Kujtim.Prenku@sqa-consulting.com>
2022-02-28 18:29:45 +0100

    Added sign-in component

--- examples/angular/src/app/app.module.ts
+++ examples/angular/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule
