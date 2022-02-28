76dbaca273b3cff6d9574152daa2fb5ab8f144c9
lewis-sqa <lewis.barnes@sqa-consulting.com>
2022-02-28 11:00:12 +0000

    Refactored props.

--- examples/vue/src/components/Content.vue
+++ examples/vue/src/components/Content.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import NearWalletSelector from "near-wallet-selector";
 import { AccountInfo } from "near-wallet-selector/lib/esm/wallets/Wallet";
 
 import Form from "./Form.vue";
 import SignIn from "./SignIn.vue";
+import Messages from "./Messages.vue";
 
 const props = defineProps<{
   selector: NearWalletSelector;
   initialAccount: AccountInfo | null;
 }>()
+
+const account = ref(props.initialAccount);
 
 const handleSignIn = () => {
   props.selector.show();
@@ -28,13 +32,14 @@ const handleSwitchProvider = () => {
 </script>
 
 <template>
-  <div v-if="initialAccount">
+  <div v-if="account">
     <button @click="handleSignOut">Log out</button>
     <button @click="handleSwitchProvider">Switch Provider</button>
   </div>
   <div v-else>
     <button @click="handleSignIn">Log in</button>
   </div>
-  <Form v-if="initialAccount" account={account} />
+  <Form v-if="account" :account="account" />
   <SignIn v-else />
+  <Messages v-if="!!account" />
 </template>
