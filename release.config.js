b21d71f0902ff6fa56a861bfb6c847490595ce9c
Rares Golea <rgolea@users.noreply.github.com>
2022-03-01 04:02:00 +0100

    removed unnecessary configs

--- release.config.js
+++ release.config.js
@@ -1,26 +1,13 @@
 module.exports = {
   branches: [
-    'main',
-    {
-      name: 'dev',
-      prerelease: true
-    }
+    'main'
   ],
   plugins: [
     '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/changelog',
-      {
-        changelogFile: 'CHANGELOG.md'
-      }
-    ],
     '@semantic-release/npm',
-    '@semantic-release/github',
     [
       '@semantic-release/git',
       {
-        assets: ['CHANGELOG.md', 'dist/**'],
         message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
       }
     ]
