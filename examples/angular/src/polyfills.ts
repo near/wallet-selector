0755ffe59980647c25e238f619161ea26798a99a
Kujtim.Prenku <Kujtim.Prenku@sqa-consulting.com>
2022-03-01 13:34:18 +0100

    Process polyfill added properly

--- examples/angular/src/polyfills.ts
+++ examples/angular/src/polyfills.ts
@@ -46,12 +46,13 @@
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js';  // Included with Angular CLI.
+import * as process from 'process';
+
+window.process = process;
 
 (window as any).global = window;
+
 global.Buffer = global.Buffer || require('buffer').Buffer;
-(window as any).process = {
-  version: ''
-};
 
 
 /***************************************************************************************************
