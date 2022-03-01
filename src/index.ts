2ba677b407c51da1fc8911ebb41a9c616387d0d7
Lewis Barnes <lewis.barnes@sqa-consulting.com>
2022-03-01 14:32:20 +0000

    Exported useful interfaces.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
 import NearWalletSelector from "./core/NearWalletSelector";
 
+export { Options } from "./core/NearWalletSelector";
+
+export {
+  Wallet,
+  WalletType,
+  BrowserWallet,
+  InjectedWallet,
+  HardwareWallet,
+  AccountInfo,
+} from "./wallets/Wallet";
+
+export {
+  Action,
+  ActionType,
+  CreateAccountAction,
+  DeployContractAction,
+  FunctionCallAction,
+  TransferAction,
+  StakeAction,
+  AddKeyAction,
+  DeleteKeyAction,
+  DeleteAccountAction,
+} from "./wallets/actions";
+
 export default NearWalletSelector;
