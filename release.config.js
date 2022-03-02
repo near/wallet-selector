aa3bd0235827c87e6cfad14d8f3a1d312cb753cf
Rares Golea <rgolea@users.noreply.github.com>
2022-03-02 09:24:49 +0100

    small changes to align better with the project

--- release.config.js
+++ release.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   branches: [
-    'main'
+    "main"
   ],
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/npm',
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/npm",
     [
-      '@semantic-release/git',
+      "@semantic-release/git",
       {
-        message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+        message: "chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
       }
     ]
   ]
