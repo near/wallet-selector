639d2a2fe14b4f9f4017bedf309a045eb1ee08e0
Kujtim.Prenku <Kujtim.Prenku@sqa-consulting.com>
2022-03-07 13:41:33 +0100

    Improved config tests

--- src/config.spec.ts
+++ src/config.spec.ts
@@ -1,24 +1,40 @@
+import { NetworkId } from "./interfaces/Options";
 import getConfig from "./config";
 
 describe("config", () => {
   it("returns the correct config for 'mainnet'", () => {
-    const networkId = "mainnet";
+    const networkId: NetworkId = "mainnet";
     const config = getConfig(networkId);
 
-    expect(config.networkId).toEqual(networkId);
+    expect(config).toEqual({
+      networkId,
+      nodeUrl: "https://rpc.mainnet.near.org",
+      walletUrl: "https://wallet.near.org",
+      helperUrl: "https://helper.mainnet.near.org",
+    });
   });
 
   it("returns the correct config for 'testnet'", () => {
-    const networkId = "testnet";
+    const networkId: NetworkId = "testnet";
     const config = getConfig(networkId);
 
-    expect(config.networkId).toEqual(networkId);
+    expect(config).toEqual({
+      networkId,
+      nodeUrl: "https://rpc.testnet.near.org",
+      walletUrl: "https://wallet.testnet.near.org",
+      helperUrl: "https://helper.testnet.near.org",
+    });
   });
 
   it("returns the correct config for 'betanet'", () => {
-    const networkId = "betanet";
+    const networkId: NetworkId = "betanet";
     const config = getConfig(networkId);
 
-    expect(config.networkId).toEqual(networkId);
+    expect(config).toEqual({
+      networkId,
+      nodeUrl: "https://rpc.betanet.near.org",
+      walletUrl: "https://wallet.betanet.near.org",
+      helperUrl: "https://helper.betanet.near.org",
+    });
   });
 });
