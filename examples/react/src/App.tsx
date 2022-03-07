a4f17576373ec5b494ce9baeed24e2c35044b636
mattlockyer <mattdlockyer@gmail.com>
2022-03-07 08:11:02 -0800

    fix example

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -6,39 +6,48 @@ import getConfig from "./config";
 import Content from "./components/Content";
 
 const App: React.FC = () => {
-  const [loaded, setLoaded] = useState(false);
-  const selectorRef = useRef<NearWalletSelector>();
+	const [loaded, setLoaded] = useState(false);
+	const selectorRef = useRef<NearWalletSelector>();
 
-  useEffect(() => {
-    const nearConfig = getConfig("testnet");
+	useEffect(() => {
+		const nearConfig = getConfig("testnet");
 
-    const selector = new NearWalletSelector({
-      wallets: ["near-wallet", "sender-wallet", "ledger-wallet"],
-      networkId: nearConfig.networkId,
-      contract: { contractId: nearConfig.contractName },
-    });
+		const selector = new NearWalletSelector({
+			wallets: ["near-wallet", "sender-wallet", "ledger-wallet"],
+			networkId: nearConfig.networkId,
+			contract: {
+				accountId: nearConfig.contractName,
+				viewMethods: [],
+				changeMethods: [],
+			},
+			walletSelectorUI: {
+				description: 'Mooo!',
+				explanation: 'Meow Woof!'
+			},
+			theme: 'dark' // or 'light'
+		});
 
-    // @ts-ignore
-    window.selector = selector;
+		// @ts-ignore
+		window.selector = selector;
 
-    selector.init().then(() => {
-      selectorRef.current = selector;
-      setLoaded(true);
-    });
+		selector.init().then(() => {
+			selectorRef.current = selector;
+			setLoaded(true);
+		});
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
-  if (!loaded) {
-    return null;
-  }
+	if (!loaded) {
+		return null;
+	}
 
-  return (
-    <Fragment>
-      <h1>NEAR Guest Book</h1>
-      <Content selector={selectorRef.current!} />
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<h1>NEAR Guest Book</h1>
+			<Content selector={selectorRef.current!} />
+		</Fragment>
+	);
 };
 
 export default App;
