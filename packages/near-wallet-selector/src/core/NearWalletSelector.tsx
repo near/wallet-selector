25656b6c60ccb7c2ebf9ce302060a87d6110d74c
Kujtim.Prenku <Kujtim.Prenku@sqa-consulting.com>
2022-03-22 19:40:17 +0100

    Set the expected type

--- packages/near-wallet-selector/src/core/NearWalletSelector.tsx
+++ packages/near-wallet-selector/src/core/NearWalletSelector.tsx
@@ -58,7 +58,7 @@ export default class NearWalletSelector {
       showModal: true,
       showWalletOptions: true,
       showLedgerDerivationPath: false,
-      showWalletNotInstalled: false,
+      showWalletNotInstalled: null,
       showSwitchNetwork: false,
     }));
   }
