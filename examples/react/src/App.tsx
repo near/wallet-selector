b8ad17870771657d95b4f41e8b1603cc89f1efc4
mattlockyer <mattdlockyer@gmail.com>
2022-03-26 10:12:26 -0700

    fix App.tsx

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -4,43 +4,6 @@ import Content from "./components/Content";
 import { WalletSelectorContextProvider } from "./contexts/WalletSelectorContext";
 
 const App: React.FC = () => {
-<<<<<<< HEAD
-	const [loaded, setLoaded] = useState(false);
-	const selectorRef = useRef<NearWalletSelector>();
-
-	useEffect(() => {
-		const nearConfig = getConfig("testnet");
-
-		const selector = new NearWalletSelector({
-			wallets: ["near-wallet", "sender-wallet", "ledger-wallet"],
-			networkId: nearConfig.networkId,
-			contract: {
-				contractId: nearConfig.contractName,
-			},
-		});
-
-		// @ts-ignore
-		window.selector = selector;
-
-		selector.init().then(() => {
-			selectorRef.current = selector;
-			setLoaded(true);
-		});
-
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
-
-	if (!loaded) {
-		return null;
-	}
-
-	return (
-		<Fragment>
-			<h1>NEAR Guest Book</h1>
-			<Content selector={selectorRef.current!} />
-		</Fragment>
-	);
-=======
   return (
     <Fragment>
       <h1>NEAR Guest Book</h1>
@@ -49,7 +12,6 @@ const App: React.FC = () => {
       </WalletSelectorContextProvider>
     </Fragment>
   );
->>>>>>> upstream/dev
 };
 
 export default App;
