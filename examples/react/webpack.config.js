0848b56e398840a92dcbee805bcf72c62a814a7b
mattlockyer <mattdlockyer@gmail.com>
2022-03-27 17:42:48 -0700

    fix lint to update spaces

--- examples/react/webpack.config.js
+++ examples/react/webpack.config.js
@@ -3,20 +3,20 @@ const nrwlConfig = require("@nrwl/react/plugins/webpack"); // require the main @
 
 module.exports = (config) => {
 
-	nrwlConfig(config); // first call it so that it @nrwl/react plugin adds its configs,
+  nrwlConfig(config); // first call it so that it @nrwl/react plugin adds its configs,
 
-	// then override your config.
-	return {
-		...config,
-		node: { global: true }, // Fix: "Uncaught ReferenceError: global is not defined", and "Can't resolve 'fs'".
-		plugins: [
-			...config.plugins,
-			new webpack.ProvidePlugin({
-				Buffer: ['buffer', 'Buffer']
-			}),
-			new webpack.DefinePlugin({
-				'process.env.NODE_ENV': JSON.stringify('production')
-			})
-		]
-	};
+  // then override your config.
+  return {
+    ...config,
+    node: { global: true }, // Fix: "Uncaught ReferenceError: global is not defined", and "Can't resolve 'fs'".
+    plugins: [
+      ...config.plugins,
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer']
+      }),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      })
+    ]
+  };
 };
