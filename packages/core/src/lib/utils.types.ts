2c4387e57f82e62e69dd140fbd414ecedc15e8f1
Lewis Barnes <lewis.barnes@sqa-consulting.com>
2022-04-08 07:43:26 +0100

    Moved to utils module.

--- packages/core/src/lib/utils.types.ts
+++ packages/core/src/lib/utils.types.ts
@@ -1,10 +1,10 @@
 export type UnpackedPromise<T> = T extends Promise<infer U> ? U : T;
 export type GenericFunction<TS extends Array<unknown>, R> = (...args: TS) => R;
 
-type Promisify<T> = {
+export type Promisify<T> = {
   [K in keyof T]: T[K] extends GenericFunction<infer TS, infer R>
     ? (...args: TS) => Promise<UnpackedPromise<R>>
     : T[K];
 };
 
-export default Promisify;
+export type Optional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
