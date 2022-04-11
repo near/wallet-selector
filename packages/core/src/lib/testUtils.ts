898d4130b856c45c381fcce6033cbf520855ec9e
Lewis Barnes <lewis.barnes@sqa-consulting.com>
2022-04-11 11:38:42 +0100

    Added test utils for wallets.

---
+++ packages/core/src/lib/testUtils.ts
@@ -0,0 +1,38 @@
+import { mock } from "jest-mock-extended";
+
+import { Wallet, WalletEvents, WalletModule } from "./wallet";
+import { Options } from "./options.types";
+import {
+  ProviderService,
+  EventEmitterService,
+  LoggerService,
+  StorageService,
+} from "./services";
+import { getNetworkPreset } from "./options";
+
+export interface MockWalletDependencies {
+  options?: Options;
+  provider?: ProviderService;
+  emitter?: EventEmitterService<WalletEvents>;
+  logger?: LoggerService;
+  storage?: StorageService;
+}
+
+export const mockWallet = <WalletVariation extends Wallet>(
+  { wallet, ...metadata }: WalletModule,
+  deps: MockWalletDependencies = {}
+) => {
+  const options = deps.options || {
+    network: getNetworkPreset("testnet"),
+    contractId: "test.testnet",
+  };
+
+  return wallet({
+    options,
+    metadata,
+    provider: deps.provider || mock<ProviderService>(),
+    emitter: deps.emitter || mock<EventEmitterService<WalletEvents>>(),
+    logger: deps.logger || mock<LoggerService>(),
+    storage: deps.storage || mock<StorageService>(),
+  }) as WalletVariation;
+};
