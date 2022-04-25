fb5e1533fd70079aa9852375de9f3b974f5dc5df
Rares Golea <rgolea@users.noreply.github.com>
2022-04-25 14:04:00 +0200

    Working stencil

---
+++ packages/selector-ui/stencil.config.ts
@@ -0,0 +1,22 @@
+import { Config } from "@stencil/core";
+
+export const config: Config = {
+  namespace: "selector-ui",
+  taskQueue: "async",
+  outputTargets: [
+    {
+      type: "dist",
+      esmLoaderPath: "../loader",
+    },
+    {
+      type: "dist-custom-elements",
+    },
+    {
+      type: "docs-readme",
+    },
+    {
+      type: "www",
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
