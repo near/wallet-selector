1163a5f0c997737c16b4f88c59234ff370440620
Lewis Barnes <lewis.barnes@sqa-consulting.com>
2022-04-29 09:52:44 +0100

    Added hardware and bridge wallets.

--- packages/core/src/lib/wallet/wallet.types.ts
+++ packages/core/src/lib/wallet/wallet.types.ts
@@ -6,9 +6,12 @@
 import { providers } from "near-api-js";
 import { AccountState } from "../store.types";
 import {
+  HardwareWalletConnectParams,
   SignAndSendTransactionParams,
   SignAndSendTransactionsParams,
 } from "./wallet";
+
+// ----- Browser Wallet ----- //
 
 export interface BrowserWalletMetadata {
   id: string;
@@ -40,6 +43,8 @@ export interface BrowserWalletModule<
   isAvailable(): Promise<boolean>;
   init(): Promise<Wallet>;
 }
+
+// ----- Injected Wallet ----- //
 
 export interface InjectedWalletMetadata {
   id: string;
@@ -77,5 +82,84 @@ export interface InjectedWalletModule<
   init(): Promise<Wallet>;
 }
 
-type WalletModule = BrowserWalletModule | InjectedWalletModule;
-type Wallet = BrowserWallet | InjectedWallet;
+// ----- Hardware Wallet ----- //
+
+export interface HardwareWalletMetadata {
+  id: string;
+  type: "hardware";
+  name: string;
+  description: string | null;
+  iconUrl: string;
+}
+
+export interface HardwareWalletBehaviour {
+  connect(params: HardwareWalletConnectParams): Promise<Array<AccountState>>;
+  disconnect(): Promise<void>;
+  getAccounts(): Promise<Array<AccountState>>;
+  signAndSendTransaction(
+    params: SignAndSendTransactionParams
+  ): Promise<providers.FinalExecutionOutcome>;
+  signAndSendTransactions(
+    params: SignAndSendTransactionsParams
+  ): Promise<Array<providers.FinalExecutionOutcome>>;
+}
+
+type HardwareWallet = HardwareWalletMetadata & HardwareWalletBehaviour;
+
+export interface HardwareWalletModule<
+  Wallet extends HardwareWallet | HardwareWalletBehaviour = HardwareWallet
+> {
+  id: string;
+  type: "hardware";
+  name: string;
+  description: string | null;
+  iconUrl: string;
+
+  isAvailable(): Promise<boolean>;
+  init(): Promise<Wallet>;
+}
+
+// ----- Bridge Wallet ----- //
+
+export interface BridgeWalletMetadata {
+  id: string;
+  type: "bridge";
+  name: string;
+  description: string | null;
+  iconUrl: string;
+}
+
+export interface BridgeWalletBehaviour {
+  connect(): Promise<Array<AccountState>>;
+  disconnect(): Promise<void>;
+  getAccounts(): Promise<Array<AccountState>>;
+  signAndSendTransaction(
+    params: SignAndSendTransactionParams
+  ): Promise<providers.FinalExecutionOutcome>;
+  signAndSendTransactions(
+    params: SignAndSendTransactionsParams
+  ): Promise<Array<providers.FinalExecutionOutcome>>;
+}
+
+type BridgeWallet = BridgeWalletMetadata & BridgeWalletBehaviour;
+
+export interface BridgeWalletModule<
+  Wallet extends BridgeWallet | BridgeWalletBehaviour = BridgeWallet
+> {
+  id: string;
+  type: "bridge";
+  name: string;
+  description: string | null;
+  iconUrl: string;
+
+  isAvailable(): Promise<boolean>;
+  init(): Promise<Wallet>;
+}
+
+type WalletModule =
+  | BrowserWalletModule
+  | InjectedWalletModule
+  | HardwareWalletModule
+  | BridgeWalletModule;
+
+type Wallet = BrowserWallet | InjectedWallet | HardwareWallet | BridgeWallet;
