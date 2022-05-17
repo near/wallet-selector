8c0879422dc29618bea929c9619630a49216b615
Lewis Barnes <lewis.barnes@sqa-consulting.com>
2022-05-17 16:37:19 +0100

    Refactored modules.

--- packages/wallet-utils/src/index.ts
+++ packages/wallet-utils/src/index.ts
@@ -1,1 +1,2 @@
-export { createAction, signTransactions } from "./lib/wallet-utils";
+export { createAction } from "./lib/create-action";
+export { signTransactions } from "./lib/sign-transactions";
