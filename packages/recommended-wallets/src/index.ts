ebcb98999676494103445139963d9ddc8b343b99
Kujtim.Prenku <Kujtim.Prenku@sqa-consulting.com>
2022-08-05 08:26:40 +0200

    Simply export setup functions of each recommended wallet

--- packages/recommended-wallets/src/index.ts
+++ packages/recommended-wallets/src/index.ts
@@ -1,6 +1,11 @@
-export { setupRecommendedWallets } from "./lib/recommended-wallets";
-export type { RecommendedWalletsParams } from "./lib/recommended-wallets.types";
+export { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
 export type { MyNearWalletParams } from "@near-wallet-selector/my-near-wallet";
+
+export { setupSender } from "@near-wallet-selector/sender";
 export type { SenderParams } from "@near-wallet-selector/sender";
+
+export { setupLedger } from "@near-wallet-selector/ledger";
 export type { LedgerParams } from "@near-wallet-selector/ledger";
+
+export { setupWalletConnect } from "@near-wallet-selector/wallet-connect";
 export type { WalletConnectParams } from "@near-wallet-selector/wallet-connect";
