954dc1cca5f39ba860923f49cfcfb0a8d1661c9b
khiemkino <87562184+khiemsoft@users.noreply.github.com>
2022-08-12 09:56:45 +0700

    Submit Coin98 Wallet

---
+++ packages/coin98-wallet/README.md
@@ -0,0 +1,7 @@
+# coin98-wallet
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test coin98-wallet` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ packages/coin98-wallet/src/lib/injected-coin98-wallet.ts
@@ -0,0 +1,17 @@
+import { Signer } from "near-api-js/lib/signer";
+
+interface IConnectParams{
+  prefix: string,
+  contractId: string
+}
+
+interface ICoin98Near{
+  account: string,
+  signer: Signer
+  connect: (params: IConnectParams) => Promise<string>,
+  disconnect: () => Promise<void>,
+}
+
+export interface InjectedCoin98{
+  near: ICoin98Near,
+}
