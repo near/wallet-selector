2dadd756bd9c5626e1afd67faba7e6ab27fcf0fd
Kujtim.Prenku <Kujtim.Prenku@sqa-consulting.com>
2022-09-02 10:55:42 +0200

    Redirect to wallet on button click.

--- packages/modal-ui-redesign/src/lib/components/WalletHome.tsx
+++ packages/modal-ui-redesign/src/lib/components/WalletHome.tsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { WhatWallet } from "./WhatWallet";
 import Icon from "../images/black-white.jpg";
-import { ModuleState, WalletSelector } from "@near-wallet-selector/core";
+import {
+  InjectedWallet,
+  ModuleState,
+  WalletSelector,
+} from "@near-wallet-selector/core";
 import { ModalHeader } from "./ModalHeader";
 import { BackArrow } from "./BackArrow";
 
@@ -34,6 +38,36 @@ export const WalletHome: React.FC<WalletHomeProps> = ({
     return () => subscription.unsubscribe();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const goToWallet = (module: ModuleState) => {
+    const { networkId } = selector.options.network;
+    let url = "";
+
+    if (module.type === "injected") {
+      url = (module as ModuleState<InjectedWallet>).metadata.downloadUrl;
+    }
+
+    // TODO: improve links to wallets other than injected type.
+    if (module.id === "my-near-wallet") {
+      const subdomain = networkId === "testnet" ? "testnet" : "app";
+      url = `https://${subdomain}.mynearwallet.com`;
+    }
+
+    if (module.id === "near-wallet") {
+      const subdomain = networkId === "testnet" ? "testnet." : "";
+      url = `https://wallet.${subdomain}near.org`;
+    }
+
+    if (
+      (url === "" && module.type === "bridge") ||
+      module.type === "hardware"
+    ) {
+      return;
+    }
+
+    window.open(url, "_blank");
+  };
+
   return (
     <div className="wallet-home-wrapper">
       <div className="nws-modal-header-wrapper">
@@ -63,7 +97,14 @@ export const WalletHome: React.FC<WalletHomeProps> = ({
                   <div className="description">{description}</div>
                 </div>
                 <div className="button-get">
-                  <button className="get-wallet">Get</button>
+                  <button
+                    className="get-wallet"
+                    onClick={() => {
+                      goToWallet(module);
+                    }}
+                  >
+                    Get
+                  </button>
                 </div>
               </div>
             );
