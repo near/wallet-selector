2438e2d903c91663dd2555d1b247ed219197008e
lostpebble <lostpebble@protonmail.com>
2022-09-02 15:05:23 +0200

    [meteor-wallet] adding verifyOwner method

--- packages/meteor-wallet/src/lib/meteor-wallet.ts
+++ packages/meteor-wallet/src/lib/meteor-wallet.ts
@@ -49,7 +49,7 @@ const setupWalletState = async (
 const createMeteorWalletInjected: WalletBehaviourFactory<
   InjectedWallet,
   { params: MeteorWalletParams_Injected }
-> = async ({ metadata, options, logger, store, params }) => {
+> = async ({ options, logger, store, params }) => {
   const _state = await setupWalletState(params, options.network);
 
   const cleanup = () => {
@@ -159,7 +159,15 @@ const createMeteorWalletInjected: WalletBehaviourFactory<
     async verifyOwner({ message }) {
       logger.log("MeteorWallet:verifyOwner", { message });
 
-      throw new Error(`Method not supported by ${metadata.name}`);
+      const response = await _state.wallet.verifyOwner({
+        message,
+      });
+
+      if (response.success) {
+        return response.payload;
+      } else {
+        throw new Error(`Couldn't verify owner: ${response.message}`);
+      }
     },
 
     async signAndSendTransaction({ signerId, receiverId, actions }) {
