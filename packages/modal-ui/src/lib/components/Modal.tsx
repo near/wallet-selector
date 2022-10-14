a165d14613496a4a88f0cd5e30353831637ec2a5
Kujtim.Prenku <Kujtim.Prenku@sqa-consulting.com>
2022-10-14 14:46:56 +0200

    Show qr code inside modal for nightly connect.

--- packages/modal-ui/src/lib/components/Modal.tsx
+++ packages/modal-ui/src/lib/components/Modal.tsx
@@ -157,7 +157,7 @@ export const Modal: React.FC<ModalProps> = ({
         params: { wallet: wallet },
       });
 
-      if (wallet.type === "bridge" && wallet.id === "wallet-connect") {
+      if (wallet.type === "bridge") {
         const subscription = selector.on("uriChanged", ({ uri }) => {
           setBridgeWalletUri(uri);
           setRoute({
