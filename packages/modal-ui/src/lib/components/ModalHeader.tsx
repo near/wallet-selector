9058a09a536c78e0b0e089b1eead03ce50f7fa55
Haris Valjevcic <haris.valjevcic@sqa-consulting.com>
2022-10-19 09:10:43 +0200

    Design changes: - Button in QR code - Background of right side of modal - Get a Wallet title centered

--- packages/modal-ui/src/lib/components/ModalHeader.tsx
+++ packages/modal-ui/src/lib/components/ModalHeader.tsx
@@ -3,16 +3,19 @@ import { CloseButton } from "./CloseButton";
 
 interface ModalHeaderProps {
   title: string;
+  isOpen: boolean;
   onCloseModal: () => void;
 }
 
 export const ModalHeader: React.FC<ModalHeaderProps> = ({
   title,
+  isOpen,
   onCloseModal,
 }) => {
+  const additionalClasses = isOpen ? "-open" : "";
   return (
     <div className="nws-modal-header">
-      <h3 className="middleTitle">{title}</h3>
+      <h3 className={`middleTitle ${additionalClasses}`}>{title}</h3>
       <CloseButton onClick={onCloseModal} />
     </div>
   );
--- packages/modal-ui/src/lib/components/WalletHome.tsx
+++ packages/modal-ui/src/lib/components/WalletHome.tsx
@@ -81,6 +81,7 @@ export const WalletHome: React.FC<WalletHomeProps> = ({
           />
         )}
         <ModalHeader
+          isOpen={route === "GetWallets" ? true : false}
           title={route === "GetWallets" ? "Get a Wallet" : "What is a Wallet?"}
           onCloseModal={onCloseModal}
         />
