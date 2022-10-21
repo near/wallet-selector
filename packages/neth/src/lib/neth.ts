743c60b49315f2daa02839ad512edbd13f3a591d
mattlockyer <mattdlockyer@gmail.com>
2022-10-21 12:43:31 -0700

    fix lint issues

--- packages/neth/src/lib/neth.ts
+++ packages/neth/src/lib/neth.ts
@@ -1,171 +1,188 @@
 import {
-	WalletModuleFactory,
-	InjectedWallet,
-	Action,
-	FunctionCallAction,
-	WalletBehaviourFactory,
-	waitFor,
+  WalletModuleFactory,
+  InjectedWallet,
+  Action,
+  FunctionCallAction,
+  WalletBehaviourFactory,
+  waitFor,
 } from "@near-wallet-selector/core";
-import detectEthereumProvider from '@metamask/detect-provider'
+import detectEthereumProvider from "@metamask/detect-provider";
 import { nearWalletIcon } from "../assets/icons";
-import { getNear, signIn, signOut, verifyOwner, signAndSendTransactions, initConnection } from "./neth-lib";
+import {
+  getNear,
+  signIn,
+  signOut,
+  verifyOwner,
+  signAndSendTransactions,
+  initConnection,
+} from "./neth-lib";
 export { initConnection } from "./neth-lib";
 
 declare global {
-	interface Window {
-		near: any,
-		ethereum: { chainId: string };
-	}
+  interface Window {
+    near: {
+      isSignedIn: () => boolean;
+    };
+    ethereum: { chainId: string };
+  }
 }
 
 export interface NethParams {
-	useModalCover?: boolean;
-	iconUrl?: string;
+  useModalCover?: boolean;
+  iconUrl?: string;
 }
 
 const isInstalled = async () => {
-	await detectEthereumProvider()
-	return !!window.ethereum;
+  await detectEthereumProvider();
+  return !!window.ethereum;
 };
 
 let useCover = false;
 
 const Neth: WalletBehaviourFactory<InjectedWallet> = async ({
-	metadata,
-	logger,
-	options,
-	provider,
+  metadata,
+  logger,
+  options,
+  provider,
 }) => {
+  initConnection(options.network);
 
-	initConnection(options.network);
+  const cover = document.createElement("div");
+  const coverImg = document.createElement("img");
+  coverImg.src = nearWalletIcon;
+  cover.className = "modal-overlay-standalone";
+  cover.style.display = "none";
+  cover.appendChild(coverImg);
+  document.body.appendChild(cover);
 
-	const cover = document.createElement("div");
-	const coverImg = document.createElement("img");
-	coverImg.src = nearWalletIcon;
-	cover.className = "modal-overlay-standalone";
-	cover.style.display = "none";
-	cover.appendChild(coverImg);
-	document.body.appendChild(cover);
+  const isValidActions = (
+    actions: Array<Action>
+  ): actions is Array<FunctionCallAction> => {
+    return actions.every((x) => x.type === "FunctionCall");
+  };
 
-	const isValidActions = (actions: Array<Action>): actions is Array<FunctionCallAction> => {
-		return actions.every((x) => x.type === "FunctionCall");
-	};
+  const transformActions = (actions: Array<Action>) => {
+    const validActions = isValidActions(actions);
 
-	const transformActions = (actions: Array<Action>) => {
-		const validActions = isValidActions(actions);
+    if (!validActions) {
+      throw new Error(
+        `Only 'FunctionCall' actions types are supported by ${metadata.name}`
+      );
+    }
 
-		if (!validActions) {
-			throw new Error(`Only 'FunctionCall' actions types are supported by ${metadata.name}`);
-		}
+    return actions.map((x) => x.params);
+  };
 
-		return actions.map((x) => x.params);
-	};
+  // return the wallet interface for wallet-selector
+  return {
+    async signIn() {
+      let account;
+      try {
+        account = await signIn();
+        if (!account) {
+          return [];
+        }
+      } catch (e) {
+        if (!/not connected/.test((e as object).toString())) {
+          throw e;
+        }
+        // console.log(e);
+      }
+      return [account];
+    },
 
-	// return the wallet interface for wallet-selector
-	return {
-		async signIn() {
-			let account;
-			try {
-				account = await signIn();
-				if (!account) return []
-			} catch (e: any) {
-				if (!/not connected/.test(e.toString())) throw e;
-				// console.log(e);
-			}
-			return [account];
-		},
+    async signOut() {
+      await signOut();
+    },
 
-		async signOut() {
-			await signOut();
-		},
+    async verifyOwner({ message }) {
+      logger.log("NETH:verifyOwner", { message });
 
-		async verifyOwner({ message }) {
-			logger.log("NETH:verifyOwner", { message });
+      verifyOwner({ message, provider, account: null });
+    },
 
-			verifyOwner({ message, provider, account: null });
-		},
+    async getAccounts() {
+      const { accountId } = await getNear();
+      return [{ accountId }];
+    },
 
-		async getAccounts() {
-			const { accountId } = await getNear();
-			return [{ accountId }];
-		},
+    async signAndSendTransaction({ receiverId, actions }) {
+      logger.log("NETH:signAndSendTransaction", {
+        receiverId,
+        actions,
+      });
 
-		async signAndSendTransaction({ receiverId, actions }) {
-			logger.log("NETH:signAndSendTransaction", {
-				receiverId,
-				actions,
-			});
+      return signAndSendTransactions({
+        transactions: [
+          {
+            receiverId,
+            actions: transformActions(actions),
+          },
+        ],
+      });
+    },
 
-			return signAndSendTransactions({
-				transactions: [
-					{
-						receiverId,
-						actions: transformActions(actions),
-					},
-				],
-			});
-		},
+    async signAndSendTransactions({ transactions }) {
+      logger.log("NETH:signAndSendTransactions", { transactions });
 
-		async signAndSendTransactions({ transactions }) {
-			logger.log("NETH:signAndSendTransactions", { transactions });
+      if (useCover) {
+        cover.style.display = "block";
+      }
 
-			if (useCover) {
-				cover.style.display = "block";
-			}
+      const transformedTxs = transactions.map(({ receiverId, actions }) => ({
+        receiverId,
+        actions: transformActions(actions),
+      }));
 
-			const transformedTxs = transactions.map(({ receiverId, actions }) => ({
-				receiverId,
-				actions: transformActions(actions),
-			}));
+      let res;
+      try {
+        res = await signAndSendTransactions({
+          transactions: transformedTxs,
+        });
+      } catch (e) {
+        /// user cancelled or near network error
+        // console.warn(e);
+      }
 
-			let res;
-			try {
-				res = await signAndSendTransactions({
-					transactions: transformedTxs,
-				});
-			} catch (e) {
-				/// user cancelled or near network error
-				console.warn(e);
-			}
+      if (useCover) {
+        cover.style.display = "none";
+      }
 
-			if (useCover) {
-				cover.style.display = "none";
-			}
-
-			return res;
-		},
-	};
+      return res;
+    },
+  };
 };
 
 export function setupNeth({
-	useModalCover = false,
-	iconUrl = nearWalletIcon,
+  useModalCover = false,
+  iconUrl = nearWalletIcon,
 }: NethParams = {}): WalletModuleFactory<InjectedWallet> {
-	return async () => {
-		// const mobile = isMobile();
-		const installed = await isInstalled();
+  return async () => {
+    // const mobile = isMobile();
+    const installed = await isInstalled();
 
-		useCover = useModalCover;
+    useCover = useModalCover;
 
-		if (!installed) {
-			return null;
-		}
+    if (!installed) {
+      return null;
+    }
 
-		await waitFor(() => !!window.near?.isSignedIn(), { timeout: 300 }).catch(() => false);
+    await waitFor(() => !!window.near?.isSignedIn(), { timeout: 300 }).catch(
+      () => false
+    );
 
-		return {
-			id: "neth",
-			type: "injected",
-			metadata: {
-				name: "NETH Account",
-				description: null,
-				iconUrl,
-				downloadUrl:
-					"https://neardefi.github.io/neth",
-				deprecated: false,
-				available: true,
-			},
-			init: Neth,
-		};
-	};
+    return {
+      id: "neth",
+      type: "injected",
+      metadata: {
+        name: "NETH Account",
+        description: null,
+        iconUrl,
+        downloadUrl: "https://neardefi.github.io/neth",
+        deprecated: false,
+        available: true,
+      },
+      init: Neth,
+    };
+  };
 }
