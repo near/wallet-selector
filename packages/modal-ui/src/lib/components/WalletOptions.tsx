2a729aef821d30d84e9ee81275f6181ea0859813
Kujtim Prenku <kujtim.prenku@sqa-consulting.com>
2022-11-16 21:50:45 +0100

    Show deprecated icon to top right corner of wallet icon.

--- packages/modal-ui/src/lib/components/WalletOptions.tsx
+++ packages/modal-ui/src/lib/components/WalletOptions.tsx
@@ -101,8 +101,8 @@ export const WalletOptions: React.FC<WalletOptionsProps> = ({
             {deprecated && (
               <div className="warning-triangle">
                 <svg
-                  width="24"
-                  height="24"
+                  width="18"
+                  height="18"
                   viewBox="0 0 24 24"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
