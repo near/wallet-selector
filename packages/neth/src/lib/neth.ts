c50dc4824affb9291dcacc2c06e653be98a02d18
amirsaran3 <amir.saran@sqa-consulting.com>
2022-11-18 14:52:33 +0100

    Removed welldone and neth wallet from mobile

--- packages/neth/src/lib/neth.ts
+++ packages/neth/src/lib/neth.ts
@@ -19,6 +19,7 @@ import {
   initConnection,
   NETH_SITE_URL,
 } from "./neth-lib";
+import isMobile from "is-mobile";
 export { initConnection } from "./neth-lib";
 
 declare global {
@@ -176,9 +177,14 @@ export function setupNeth({
     customGas = gas;
     bundle = _bundle;
 
+    const mobile = isMobile();
     const installed = await isInstalled();
 
     await waitFor(() => !!isSignedIn(), { timeout: 300 }).catch(() => false);
+
+    if (mobile) {
+      return null;
+    }
 
     return {
       id: "neth",
