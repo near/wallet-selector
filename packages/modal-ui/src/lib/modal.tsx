02dabfe6d7665424fece2a421cb74bc7a504afb8
amirsaran3 <amir.saran@sqa-consulting.com>
2022-11-26 16:51:44 +0100

    Fixed modal-ui modal rerender

--- packages/modal-ui/src/lib/modal.tsx
+++ packages/modal-ui/src/lib/modal.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import type { Root } from "react-dom/client";
 import { createRoot } from "react-dom/client";
 import type { WalletSelector } from "@near-wallet-selector/core";
 
@@ -7,21 +8,23 @@ import { Modal } from "./components/Modal";
 
 const MODAL_ELEMENT_ID = "near-wallet-selector-modal";
 
-let modalInstance: WalletSelectorModal | null = null;
+let rootInstance: Root | null = null;
 
 export const setupModal = (
   selector: WalletSelector,
   options: ModalOptions
 ): WalletSelectorModal => {
-  const el = document.createElement("div");
-  el.id = MODAL_ELEMENT_ID;
-  document.body.appendChild(el);
+  if (!rootInstance) {
+    const el = document.createElement("div");
+    el.id = MODAL_ELEMENT_ID;
+    document.body.appendChild(el);
 
-  const container = document.getElementById(MODAL_ELEMENT_ID);
-  const root = createRoot(container!);
+    const container = document.getElementById(MODAL_ELEMENT_ID);
+    rootInstance = createRoot(container!);
+  }
 
   const render = (visible = false) => {
-    root.render(
+    rootInstance!.render(
       <Modal
         selector={selector}
         options={options}
@@ -33,16 +36,12 @@ export const setupModal = (
 
   render();
 
-  if (!modalInstance) {
-    modalInstance = {
-      show: () => {
-        render(true);
-      },
-      hide: () => {
-        render(false);
-      },
-    };
-  }
-
-  return modalInstance;
+  return {
+    show: () => {
+      render(true);
+    },
+    hide: () => {
+      render(false);
+    },
+  };
 };
