e5b27200d3113070c254d38a279056df15b8e864
andreapn <fifi.hate.lonely@gmail.com>
2022-12-22 20:05:48 +0700

    minor fix neth not show on metamask mobile

--- packages/neth/src/lib/neth.ts
+++ packages/neth/src/lib/neth.ts
@@ -19,7 +19,7 @@ import {
   initConnection,
   NETH_SITE_URL,
 } from "./neth-lib";
-import isMobile from "is-mobile";
+// import isMobile from "is-mobile";
 export { initConnection } from "./neth-lib";
 
 declare global {
@@ -177,14 +177,14 @@ export function setupNeth({
     customGas = gas;
     bundle = _bundle;
 
-    const mobile = isMobile();
+    // const mobile = isMobile();
     const installed = await isInstalled();
 
     await waitFor(() => !!isSignedIn(), { timeout: 300 }).catch(() => false);
 
-    if (mobile) {
-      return null;
-    }
+    // if (mobile) {
+    //   return null;
+    // }
 
     return {
       id: "neth",
