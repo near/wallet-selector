41b1fb3bd01f36d7816cc38a47b4f4e60077dbb4
Kujtim Prenku <kujtim.prenku@sqa-consulting.com>
2023-02-01 13:53:04 +0100

    Added loading component.

---
+++ examples/angular/src/app/components/loading/loading.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "near-wallet-selector-loading",
+  templateUrl: "./loading.component.html",
+  styleUrls: ["./loading.component.scss"],
+})
+export class LoadingComponent {}
