9ea7d960e6eec952e277a0951f6fc07d44cd48cb
DamirSQA <damir.pjetlovic@sqa-consulting.com>
2023-02-01 16:17:19 +0100

    removed unused component

--- examples/angular/src/app/components/export-account/export-account.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "near-wallet-selector-export-account",
-  templateUrl: "./export-account.component.html",
-  styleUrls: ["./export-account.component.scss"],
-})
-export class ExportAccountComponent {}
