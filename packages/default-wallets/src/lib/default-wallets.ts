e8c43401eb0298fc029539721dc636ee6cd278e4
Kujtim Prenku <kujtim.prenku@sqa-consulting.com>
2023-02-02 11:17:42 +0100

    Removed setupMyNearWallet and setupLedger from default wallets list.

--- packages/default-wallets/src/lib/default-wallets.ts
+++ packages/default-wallets/src/lib/default-wallets.ts
@@ -1,9 +1,7 @@
-import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
-import { setupLedger } from "@near-wallet-selector/ledger";
 import type { WalletModuleFactory } from "@near-wallet-selector/core";
 
 export const setupDefaultWallets = async (): Promise<
   Array<WalletModuleFactory>
 > => {
-  return [setupMyNearWallet(), setupLedger()];
+  return [];
 };
