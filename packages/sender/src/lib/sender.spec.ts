79e06d634618bd191d92ea2e7fb9932c8292841c
amirsaran3 <amir.saran@sqa-consulting.com>
2023-03-10 14:12:48 +0100

    removed console.logs

--- packages/sender/src/lib/sender.spec.ts
+++ packages/sender/src/lib/sender.spec.ts
@@ -4,15 +4,9 @@ import type { MockWalletDependencies } from "../../../core/src/lib/testUtils";
 import type { InjectedWallet } from "../../../core/src/lib/wallet";
 
 const createSenderWallet = async (deps: MockWalletDependencies = {}) => {
-  // eslint-disable-next-line no-console
-  console.log("asdasdasdasd");
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const { setupSender } = require("./sender");
-  // eslint-disable-next-line no-console
-  console.log("setupWallet", setupSender);
   const { wallet } = await mockWallet<InjectedWallet>(setupSender(), deps);
-  // eslint-disable-next-line no-console
-  console.log("wallet", wallet);
 
   return {
     wallet,
@@ -25,14 +19,9 @@ afterEach(() => {
 
 describe("signIn", () => {
   it("sign into near wallet", async () => {
-    // eslint-disable-next-line no-console
-    console.log("========================================asdasdasd");
     const { wallet } = await createSenderWallet();
 
-    const res = await wallet.signIn({ contractId: "test.testnet" });
-
-    // eslint-disable-next-line no-console
-    console.log("===================", res);
+    await wallet.signIn({ contractId: "test.testnet" });
 
     // expect(window.near.requestSignIn).toHaveBeenCalled();
   });
