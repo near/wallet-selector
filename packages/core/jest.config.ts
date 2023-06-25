0ae86c77eae1953d624739f8098fc74d355a1dbb
kujtimprenkuSQA <kujtim.prenku@sqa-consulting.com>
2023-06-25 11:48:22 +0200

    Migrate NX workspace to v16.0.0

--- packages/core/jest.config.ts
+++ packages/core/jest.config.ts
@@ -1,16 +1,17 @@
 /* eslint-disable */
 export default {
-  displayName: 'core',
-  preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
-  testEnvironment: 'node',
+  displayName: "core",
+  preset: "../../jest.preset.js",
+  globals: {},
+  testEnvironment: "node",
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest',
+    "^.+\\.[tj]sx?$": [
+      "ts-jest",
+      {
+        tsconfig: "<rootDir>/tsconfig.spec.json",
+      },
+    ],
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/packages/core',
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  coverageDirectory: "../../coverage/packages/core",
 };
