f7fa122f9314fa234f4207360ba279a3154c8e74
Erdit Kurteshi <erdit.kurteshi@sqa-consulting.com>
2023-09-20 13:40:08 +0200

    added isInstall method for near-snap

--- packages/near-snap/src/lib/index.ts
+++ packages/near-snap/src/lib/index.ts
@@ -4,11 +4,37 @@ import icon from "./icon";
 
 export { icon };
 
+declare global {
+  interface Window {
+    ethereum: {
+      chainId: string;
+      // eslint-disable-next-line
+      request: any;
+    };
+  }
+}
+
+const isInstalled = async (): Promise<boolean> => {
+  try {
+    const provider = window.ethereum;
+    const clientVersion = await provider?.request({
+      method: "web3_clientVersion",
+    });
+
+    const isFlaskDetected = (clientVersion as Array<string>)?.includes("flask");
+    return Boolean(provider && isFlaskDetected);
+  } catch {
+    return false;
+  }
+};
+
 export function setupNearSnap({
   deprecated = false,
   iconUrl = icon,
 } = {}): WalletModuleFactory {
   return async () => {
+    const installed = await isInstalled();
+
     return {
       id: "near-snap",
       type: "injected",
@@ -19,7 +45,7 @@ export function setupNearSnap({
         downloadUrl: "https://near-snap.surge.sh",
         iconUrl,
         deprecated,
-        available: true,
+        available: installed,
       },
     };
   };
