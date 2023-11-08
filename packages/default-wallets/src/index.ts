37200c48d905fa1f7e97b6005d9faa3ef8664768
kujtimprenkuSQA <kujtim.prenku@sqa-consulting.com>
2023-11-08 19:25:33 +0100

    Remove default-wallets package.

--- packages/default-wallets/src/index.ts
+++
@@ -1,1 +0,0 @@
-export { setupDefaultWallets } from "./lib/default-wallets";
--- packages/default-wallets/src/lib/default-wallets.ts
+++
@@ -1,7 +0,0 @@
-import type { WalletModuleFactory } from "@near-wallet-selector/core";
-
-export const setupDefaultWallets = async (): Promise<
-  Array<WalletModuleFactory>
-> => {
-  return [];
-};
