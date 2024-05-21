27540eee3505985b368a704d70ccc2ff83e0b72d
Pierre-Alain Ouvrard <pierre-alain.ouvrard@aurora.dev>
2024-05-21 15:03:30 +0900

    style: Modal lint.

--- packages/ethereum-wallets/src/lib/modal.ts
+++ packages/ethereum-wallets/src/lib/modal.ts
@@ -1,8 +1,4 @@
-import type {
-  FunctionCallAction,
-  Transaction,
-  TransferAction,
-} from "@near-wallet-selector/core";
+import type { Transaction } from "@near-wallet-selector/core";
 import { formatUnits } from "viem";
 import { DEFAULT_ACCESS_KEY_ALLOWANCE, RLP_EXECUTE } from "./utils";
 
@@ -149,46 +145,58 @@ export function createModal({
       }
       txElement.innerHTML = `
         ${
+          // Transaction description
           tx.actions[0].type === "AddKey"
             ? tx.actions[0].params.accessKey.permission === "FullAccess"
-              ? "WARNING: The application is requesting a FullAccess key, you can loose your account and all your assets, only approve this transaction if you know what you are doing !!!"
-              : `<p>Connect to ${
-                  tx.actions[0].params.accessKey.permission.receiverId
-                }</p>
-            <p>Network Fee Allowance: ${
-              tx.actions[0].params.accessKey.permission.allowance === "0"
-                ? "unlimited"
-                : formatUnits(
-                    BigInt(
-                      tx.actions[0].params.accessKey.permission.allowance ??
-                        DEFAULT_ACCESS_KEY_ALLOWANCE
-                    ),
-                    24
-                  )
-            } NEAR</p>
-            <p>This allowance is spendable by the application towards network fees incurred during use.</p>
-            `
+              ? "<p>WARNING: The application is requesting a FullAccess key, you can loose your account and all your assets, only approve this transaction if you know what you are doing !!!<p>"
+              : `
+                  <p>Connect to ${
+                    tx.actions[0].params.accessKey.permission.receiverId
+                  }</p>
+                  <p>
+                    Network Fee Allowance: ${
+                      tx.actions[0].params.accessKey.permission.allowance ===
+                      "0"
+                        ? "unlimited"
+                        : formatUnits(
+                            BigInt(
+                              tx.actions[0].params.accessKey.permission
+                                .allowance ?? DEFAULT_ACCESS_KEY_ALLOWANCE
+                            ),
+                            24
+                          )
+                    } NEAR
+                  </p>
+                  <p>This allowance is spendable by the application towards network fees incurred during use.</p>
+                `
             : tx.actions[0].type === "DeleteKey"
-            ? "This is an optional transaction which removes the application access key. If you reject the transaction, the key will be reused when you login again."
+            ? "<p>This is an optional transaction which removes the application access key. If you reject the transaction, the key will be reused when you login again.</p>"
             : tx.actions[0].type === "FunctionCall"
             ? `
-              <p>Contract execution:</p>
-              <p>from: ${tx.signerId}</p>
-              <p>to: ${tx.receiverId}</p>
-              <p>function: ${tx.actions[0].params.methodName}</p>
-              <p>deposit: ${formatUnits(
-                BigInt(tx.actions[0].params.deposit),
-                24
-              )} NEAR</p>
+                <p>Contract execution:</p>
+                <p>from: ${tx.signerId}</p>
+                <p>to: ${tx.receiverId}</p>
+                <p>function: ${tx.actions[0].params.methodName}</p>
+                <p>
+                  deposit: ${formatUnits(
+                    BigInt(tx.actions[0].params.deposit),
+                    24
+                  )} NEAR
+                </p>
             `
             : tx.actions[0].type === "Transfer"
-            ? `Transfer ${formatUnits(
-                BigInt(tx.actions[0].params.deposit),
-                24
-              )} NEAR from ${tx.signerId} to ${tx.receiverId}`
+            ? `
+                <p>
+                  Transfer ${formatUnits(
+                    BigInt(tx.actions[0].params.deposit),
+                    24
+                  )} NEAR from ${tx.signerId} to ${tx.receiverId}
+                </p>
+              `
             : "Unknown transaction type."
         }
         ${
+          // Relayer onboarding
           tx.actions[0].type === "AddKey" &&
           tx.actions[0].params.accessKey.permission !== "FullAccess" &&
           tx.actions[0].params.accessKey.permission.allowance === "0"
@@ -199,19 +207,21 @@ export function createModal({
                 1 &&
               tx.actions[0].params.accessKey.permission.methodNames[0] ===
                 RLP_EXECUTE
-              ? "This transaction will onboard your account and enable you to send the next transactions."
-              : "WARNING: this key has unlimited allowance and can spend all your NEAR, only approve this transaction if you know what you are doing !!!"
+              ? "<p>This transaction will onboard your account and enable you to send the next transactions.</p>"
+              : "<p>WARNING: this key has unlimited allowance and can spend all your NEAR, only approve this transaction if you know what you are doing !!!</p>"
             : ""
         }
         <p>Transaction Details:</p>
         <p>${JSON.stringify(tx.actions[0], null, 2)}</p>
-        <p>Status: ${
-          i < selectedIndex
-            ? "completed"
-            : i === selectedIndex
-            ? "sign the transaction in your wallet..."
-            : "pending..."
-        }</p>
+        <p>
+          Status: ${
+            i < selectedIndex
+              ? "completed"
+              : i === selectedIndex
+              ? "sign the transaction in your wallet..."
+              : "pending..."
+          }
+        </p>
       `;
       container.appendChild(txElement);
     });
