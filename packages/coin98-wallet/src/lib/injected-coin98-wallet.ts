b78e5c8d1e3b5f04389738d7fb56a05307b23318
thanghoduc <hoducthangtn2nhvt@gmail.com>
2024-07-25 17:14:36 +0700

    fix: comment commit

--- packages/coin98-wallet/src/lib/injected-coin98-wallet.ts
+++ packages/coin98-wallet/src/lib/injected-coin98-wallet.ts
@@ -1,4 +1,4 @@
-import { SignedMessage, SignMessageParams } from "@near-wallet-selector/core";
+import type { SignedMessage, SignMessageParams } from "@near-wallet-selector/core";
 import type { Signer } from "near-api-js/lib/signer";
 
 interface IConnectParams {
