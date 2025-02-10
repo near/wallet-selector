32faf3d3ef33e9686223f67c40fb6e894eaedc2e
trechriron <trechriron@gmail.com>
2025-02-10 13:42:22 -0800

    Added script for easy publish of packages to set scope public

---
+++ scripts/release-package-public.bash
@@ -0,0 +1,5 @@
+OTP=112233
+TAG=latest
+
+npm publish dist/packages/hot-wallet --tag "${TAG}" --otp "${OTP}" --access public
+npm publish dist/packages/react-hook --tag "${TAG}" --otp "${OTP}" --access public
