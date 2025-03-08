af39db701b8a6bcdf39a87d89f45f66fcf95fc98
Guillermo Alejandro Gallardo Diez <gagdiez@MacBookPro.home>
2025-03-08 18:57:39 +0100

    chore: remove dependency on big.js

--- examples/angular/src/app/components/form/form.component.ts
+++ examples/angular/src/app/components/form/form.component.ts
@@ -1,6 +1,5 @@
 import type { OnInit } from "@angular/core";
 import { Component, EventEmitter, Input, Output } from "@angular/core";
-import Big from "big.js";
 import { Account } from "../../interfaces/account";
 
 export type Submitted = SubmitEvent & {
@@ -12,16 +11,9 @@ export type Submitted = SubmitEvent & {
   templateUrl: "./form.component.html",
   styleUrls: ["./form.component.scss"],
 })
-export class FormComponent implements OnInit {
+export class FormComponent {
   @Input() account: Account;
   @Output() addMessage: EventEmitter<Submitted> = new EventEmitter();
-  maxValue: string;
-
-  ngOnInit(): void {
-    this.maxValue = Big(this.account.amount)
-      .div(10 ** 24)
-      .toString();
-  }
 
   onSubmit(event: Submitted) {
     this.addMessage.emit(event);
