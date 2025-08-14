3bfd14c76ca60a414abb59bda3ad1e4bdadab18b
Paul Myburgh <lostpebble@protonmail.com>
2025-08-14 17:07:48 +0200

    fix: have meteor working with newest near libs and example app

---
+++ packages/meteor-wallet/src/lib/nullEmpty.ts
@@ -0,0 +1,19 @@
+export const notNullEmpty = (str: string | null | undefined): str is string => {
+  return str != null && str.length > 0;
+};
+
+export const nullEmpty = (str: string | null | undefined): str is null | undefined | "" => {
+  return !notNullEmpty(str);
+};
+
+export const firstNotNullEmpty = (
+  ...strItems: (string | null | undefined)[]
+): string | undefined => {
+  for (const item of strItems) {
+    if (notNullEmpty(item)) {
+      return item;
+    }
+  }
+
+  return undefined;
+};
