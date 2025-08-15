db204c58cc68489f2caa06f72abe19bdec129f45
Paul Myburgh <lostpebble@protonmail.com>
2025-08-15 08:36:59 +0200

    fix: lint errors

--- packages/meteor-wallet/src/lib/nullEmpty.ts
+++ packages/meteor-wallet/src/lib/nullEmpty.ts
@@ -2,12 +2,14 @@ export const notNullEmpty = (str: string | null | undefined): str is string => {
   return str != null && str.length > 0;
 };
 
-export const nullEmpty = (str: string | null | undefined): str is null | undefined | "" => {
+export const nullEmpty = (
+  str: string | null | undefined
+): str is null | undefined | "" => {
   return !notNullEmpty(str);
 };
 
 export const firstNotNullEmpty = (
-  ...strItems: (string | null | undefined)[]
+  ...strItems: Array<string | null | undefined>
 ): string | undefined => {
   for (const item of strItems) {
     if (notNullEmpty(item)) {
