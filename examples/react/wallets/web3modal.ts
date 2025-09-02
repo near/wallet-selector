daf8e830173571f33de57591aa5f8e6a0aa548a1
Guille <gagdiez.c@gmail.com>
2025-09-02 18:31:30 +0200

    Apply suggestions from code review

--- examples/react/wallets/web3modal.ts
+++ examples/react/wallets/web3modal.ts
@@ -19,8 +19,8 @@ const near = defineChain({
     symbol: "NEAR",
   },
   rpcUrls: {
-    default: { http: ["https://test.rpc.fastnear.com"] },
-    public: { http: ["https://test.rpc.fastnear.com"] },
+    default: { http: ["https://eth-rpc.testnet.near.org"] },
+    public: { http: ["https://eth-rpc.testnet.near.org"] },
   },
   blockExplorers: {
     default: {
